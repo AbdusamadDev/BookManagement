46fa4d952df73a1b3de6f656392c41437253584d
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-11 18:17:16 -0500

    CLI Commit

--- api/models.py
+++ api/models.py
@@ -75,14 +75,18 @@ class Database:
         start = time.time()
         if self.name is None:
             raise TypeError("name parameter is not provided")
-        columns = kwargs
+        keys = list(kwargs.keys())
         def validate_fields():
-            key = kwargs.pop()
+            index = 0
+            key = keys[index]
+            print(key)
+            index += 1
             if key not in self.columns:
                 raise Exception(f"Invalid column name: {key}")
             return validate_fields()
-        fields = str(list(columns.keys()))[1:-1].replace('"', "").replace("'", "")
-        values = str(list(columns.values()))[1:-1]
+        validate_fields()
+        fields = str(list(kwargs.keys()))[1:-1].replace('"', "").replace("'", "")
+        values = str(list(kwargs.values()))[1:-1]
         self.cursor.execute(f"""INSERT INTO {self.name} ({fields}) VALUES ({values})""")
         self.commit()
         end = time.time()
@@ -108,5 +112,5 @@ class Database:
 if __name__ == "__main__":
     database = Database(name="new_table", fields={"name": "Text", "age": "Integer"})
     database.createdb()
-    database.add(name="Abdusamad", agess=18)
+    database.add(name="Abdusamad", age=18)
     print(database.columns)
