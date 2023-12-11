aca4df46cc966d7db7af70950868c1f9fe9fee9b
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-11 13:14:14 -0800

    CLI Commit

--- api/models.py
+++ api/models.py
@@ -5,18 +5,28 @@ import psycopg2
 
 class Connection:
     def __init__(
-        self,
-        database: str,
-        host: str,
-        port: int,
-        user: str,
+        self, database: str, host: str, port: int, user: str, password: str
     ) -> None:
-        pass
+        self.database = database
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+
+    def create_table(table):
+
 
 
 class Table:
     def __init__(self, name, **fields) -> None:
         self.name = name
         self.fields = fields
+        self.connection = Connection()
+
+    def create(self):
+        pass
+
+    de
+
 
     # def create(self, )
