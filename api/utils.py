c7259db01a90cd222cbdbb3178dc92739c19420d
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-12 02:16:35 -0800

    CLI commit 3

--- api/utils.py
+++ api/utils.py
@@ -1,4 +1,5 @@
 from exceptions import ValidationError, AuthenticationError
+from models import Database
 
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
@@ -45,12 +46,18 @@ def decode_token(token):
 
 
 def authenticate_or_abort(token):
-    decoded_payload = decode_token(token)
-    if "username" in decoded_payload.keys() and "password" in decoded_payload.keys():
-        user = 
-    return AuthenticationError(
+    base_exception = AuthenticationError(
         description="Authentication credentials failed", status=401
     )
+    decoded_payload = decode_token(token)
+    if "username" in decoded_payload.keys() and "password" in decoded_payload.keys():
+        model = Database(name="users")
+        user = model.get(username=decoded_payload.get("username"))
+        if user:
+            is_correct_passwd = verify_pwd(decoded_payload.get("password"), user[-1])
+            if not is_correct_passwd:
+                return base_exception
+    return base_exception
 
 
 if __name__ == "__main__":
