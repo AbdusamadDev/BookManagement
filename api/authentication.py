65b63d8f155073ee2b67395b6d0934616dc5d8fe
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-11 16:23:40 -0800

    CLI Commit

--- api/authentication.py
+++ api/authentication.py
@@ -8,9 +8,11 @@ auth_route = Blueprint("auth", __name__)
 @auth_route.post("/users/new/")
 def register():
     data = request.get_json()
+    # Validation process goes for valid json data
     if "username" not in data.keys():
         return ValidationError(description="Username was not provided!")
     if "email" not in data.keys():
-        if 
+        vl_email = data.get("email")
+        if "@" not in vl_email or "." not in vl_email:
+            return ValidationError(description="Invalid Email provided!")
         return ValidationError(description="Email is not provided!")
-    
