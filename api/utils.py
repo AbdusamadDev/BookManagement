844a7fe64bd67b080b07a7ab3d360c8a2ad14ca1
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-11 16:28:42 -0800

    CLI Commit

--- api/utils.py
+++ api/utils.py
@@ -1,1 +1,18 @@
-from bcrypt import 
+import bcrypt
+
+
+def hash_pwd(passwd):
+    # hashing password for user security
+    bytes = passwd.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(bytes, salt)
+    return hashed_pwd
+
+
+def verify_pwd(passwd, hashed_pwd):
+    bytes = passwd.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    userBytes = hashed_pwd.encode("utf-8")
+    result = bcrypt.checkpw(userBytes, hash)
+    return result
