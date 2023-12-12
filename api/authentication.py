f87b5ba20d5b043092bc17f4aa7b1bd09093509c
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-11 16:58:56 -0800

    CLI Commit

--- api/authentication.py
+++ api/authentication.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request
-from exceptions import ValidationError
-
+from flask import Blueprint, request, Response
+from exceptions import ValidationError, DatabaseError
+from models import Database
+from utils import hash_pwd
 
 auth_route = Blueprint("auth", __name__)
 
@@ -13,12 +14,33 @@ def register():
         "username": "Username was not provided",
         "email": "Email was not provided",
         "password": "Password was not provided",
-        "confirm_password": "Confirm of password was not passed"
+        "confirm_password": "Confirm of password was not passed",
     }
     for key, value in requirements.items():
         if key not in data.keys():
             return ValidationError(value)
-    email, username, password, confirm_password = data.get("email"), d
-    if "@" not in vl_email or "." not in vl_email:
+    email, username, password, confirm_password = (
+        data.get("email"),
+        data.get("username"),
+        data.get("password"),
+        data.get("confirm_password"),
+    )
+    if "@" not in email or "." not in email:
         return ValidationError(description="Invalid Email provided!")
+    if password != confirm_password:
+        return ValidationError("Passwords didn't match!")
+
+    try:
+        user = Database(
+            "users",
+            fields={
+                "username": "TEXT NOT NULL",
+                "email": "TEXT UNIQUE NOT NULL",
+                "password": "TEXT",
+            },
+        )
+        user.add(username=username, email=email, password=hash_pwd(password))
+    except Exception as body:
+        return DatabaseError(description=str(body), status=422)
     
+    return Response()
