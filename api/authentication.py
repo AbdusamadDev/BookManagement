46fe92e514fcb8d7d71299be78040d7dc4d23b23
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-13 00:08:20 -0800

    CLI commit 2

--- api/authentication.py
+++ api/authentication.py
@@ -3,20 +3,17 @@ from utils import decode_token, verify_pwd
 from models import Database
 
 
-def authenticate_or_abort(token):
-    base_exception = AuthenticationError(
-        description="Authentication credentials failed", status=401
-    )
-    if token is None:
-        return AuthenticationError(description="Token is not provided!", status=401)
+def is_authenticated(token):
     decoded_payload = decode_token(token)
+    if not token
     if "username" in decoded_payload.keys() and "password" in decoded_payload.keys():
         model = Database(name="users")
         user = model.get(username=decoded_payload.get("username"))
         if user:
             is_correct_passwd = verify_pwd(decoded_payload.get("password"), user[-1])
             if not is_correct_passwd:
-                return base_exception
+                return False
+            return True
         else:
-            return base_exception
-    return base_exception
+            return False
+    return False
--- api/bms.py
+++ api/bms.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from authentication import authenticate_or_abort
+from authentication import is_authenticated
 
 
 bms_route = Blueprint("Book Management Service", __name__)
@@ -9,6 +9,6 @@ bms_route = Blueprint("Book Management Service", __name__)
 def create():
     token = request.headers.get("token", None)
     print(token)
-    authenticate_or_abort(to)
+    is_authenticated(token)
     # Now request is trusted and authenticated
     return "Authenticated successfully!"
