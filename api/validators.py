073d2efd3a56459b51d824fe86031ea3d8c60a5f
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-15 07:32:31 -0800

    CLI commit 19

--- api/validators.py
+++ api/validators.py
@@ -1,5 +1,5 @@
 def validate_fields(*columns, **request_data):
     # mustnt be null or blank
     # must include all fields
-    for key, value in request_data.items():
+    for field in columns:
         
