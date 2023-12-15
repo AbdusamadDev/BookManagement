77be36873c8bd35f14c68c6e3012d0641f6a287a
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-15 02:14:44 -0800

    CLI commit 3

--- api/bms.py
+++ api/bms.py
@@ -29,8 +29,9 @@ books.createdb()
 def create():
     # Authentication
     token = request.headers.get("Authorization", None)
+    if request.get_data():
+        data = request.get_data()   
     data = request.get_json()
-    print(token)
     if not is_authenticated(token):
         return AuthenticationError(description="Not authenticated", status=401)
     else:
